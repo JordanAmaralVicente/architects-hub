eebba6dd3809ce29ab222dd09ea1127f203a16d0
JordanAmaralVicente <jordanitalovicente@gmail.com>
2022-12-27 20:12:13 -0300

    feature(server): create basic structure to the server

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { OrdersModule } from './features/orders/orders.module';
+import { UsersModule } from './features/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, OrdersModule],
 })
 export class AppModule {}
