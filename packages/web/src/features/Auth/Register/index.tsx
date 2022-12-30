8f6630e34d163a2823fce680dbd833b5a699e71d
JordanAmaralVicente <jordanitalovicente@gmail.com>
2022-12-30 11:51:19 -0300

    fix(web/register): fix register page behavior on mobile

--- packages/web/src/features/Auth/Register/index.tsx
+++ packages/web/src/features/Auth/Register/index.tsx
@@ -1,12 +1,17 @@
 import { Box, styled } from "@mui/material";
 import { Form } from "./components";
 
-export const OuterPageContainer = styled(Box)(() => ({
+export const OuterPageContainer = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   width: "100%",
   justifyContent: "center",
   alignItems: "center",
+  marginTop: "72px",
+
+  [theme.breakpoints.down("sm")]: {
+    marginTop: "16px",
+  },
 }));
 
 export function RegisterPage(): JSX.Element {
