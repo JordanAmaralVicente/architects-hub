ff6519c039e9f2eb2609168e66bd8f64c1865fdb
JordanAmaralVicente <jordanitalovicente@gmail.com>
2022-12-30 08:02:19 -0300

    feat(front/components): create order status badge

--- packages/web/src/components/OrderCard/index.tsx
+++ packages/web/src/components/OrderCard/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Paper, styled, Typography } from "@mui/material";
 import { Order } from "../../types/order";
-import { OrderStatusesMap } from "../../types/order-status";
+import { OrderStatus, OrderStatusesMap } from "../../types/order-status";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "white",
@@ -18,6 +18,15 @@ interface OrderCardProps {
   order: Order;
   onClick?: (id: string) => void;
 }
+
+const statusColor = new Map<
+  OrderStatus,
+  { backgroundColor: string; color: string }
+>([
+  [OrderStatus.ACCEPTED, { backgroundColor: "#adffb1", color: "#388e3c" }],
+  [OrderStatus.REFUSED, { backgroundColor: "#f5a2a2", color: "#d32f2f" }],
+  [OrderStatus.SOLICITED, { backgroundColor: "#97daf7", color: "#0288d1" }],
+]);
 
 export const OrderCard = (props: OrderCardProps): JSX.Element => {
   return (
@@ -47,7 +56,17 @@ export const OrderCard = (props: OrderCardProps): JSX.Element => {
         {props.order.description}
       </Typography>
       <Box>
-        <Typography>{OrderStatusesMap.get(props.order.status)}</Typography>
+        <span
+          style={{
+            ...statusColor.get(props.order.status),
+            borderRadius: "3px",
+            padding: "6px",
+            fontSize: "14px",
+            fontWeight: "bold",
+          }}
+        >
+          {OrderStatusesMap.get(props.order.status)}
+        </span>
       </Box>
     </Item>
   );
